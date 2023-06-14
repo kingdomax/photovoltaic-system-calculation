bc6c3aebcd5dee76b4f095d76e2071faf973d858
kingdomax <kingdomax@windowslive.com>
2023-06-14 05:28:45 +0200

    initialize project

---
+++ frontend/src/js/main.js
@@ -0,0 +1,3 @@
+import '../scss/styles.scss'; // Import our custom CSS
+import * as bootstrap from 'bootstrap'; // Import all of Bootstrap's JS
+//import { Tooltip, Toast, Popover } from 'bootstrap'; // or, specify which plugins you need:
