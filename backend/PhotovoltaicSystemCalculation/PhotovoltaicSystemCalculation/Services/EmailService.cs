55a6bd766c79dd16867bc10f7409a04abde0d03c
kingdomax <kingdomax@windowslive.com>
2023-07-01 19:26:13 +0200

    Fix send email problem

--- backend/PhotovoltaicSystemCalculation/PhotovoltaicSystemCalculation/Services/EmailService.cs
+++ backend/PhotovoltaicSystemCalculation/PhotovoltaicSystemCalculation/Services/EmailService.cs
@@ -18,19 +18,114 @@ namespace PhotovoltaicSystemCalculation.Services
             _userAccountRepository = userAccountRepository;
         }
 
+        //public async Task<bool> SendReport(IList<ReportData> reportData, int userId, string projectName)
+        //{
+        //    string fileName;
+
+        //    //Get recipient Email and project name
+        //    UserDTO user = await _userAccountRepository.GetUser(userId);
+        //    string email = user.Email;
+
+        //    StringBuilder emailBody = new StringBuilder();
+        //    emailBody.AppendLine($"Report 30 Days Electric Produce of All products of the Project: {projectName}");
+
+        //    //Generate Excel file, grouping product by lat and long
+        //    ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+        //    using (var package = new ExcelPackage())
+        //    {
+        //        var groupedReportData = reportData.GroupBy(r => new { r.Product.Id });
+
+        //        foreach (var group in groupedReportData)
+        //        {
+        //            var firstItemInGroup = group.First();
+        //            var ws = package.Workbook.Worksheets.Add($"Product ID:{firstItemInGroup.Product.Id} {firstItemInGroup.Product.Brand}");
+
+        //            ws.Cells["A1"].Value = "ProductName";
+        //            ws.Cells["B1"].Value = "Latitude";
+        //            ws.Cells["C1"].Value = "Longitude";
+        //            ws.Cells["D1"].Value = "Date";
+        //            ws.Cells["E1"].Value = "Electric Production";
+
+        //            int rowNumber = 2;
+        //            foreach (var reportDataItem in group)
+        //            {
+        //                Product product = reportDataItem.Product;
+        //                string productName = product.Name;
+        //                string latitude = product.Latitude.ToString(CultureInfo.InvariantCulture);
+        //                string longitude = product.Longitude.ToString(CultureInfo.InvariantCulture);
+
+        //                emailBody.AppendLine();
+        //                emailBody.AppendLine($"Product: {productName}, Latitude: {latitude}, Longitude: {longitude}");
+
+        //                IList<ElectricProduction> electricProductions = reportDataItem.ElectricProductions;
+        //                foreach (ElectricProduction electricProduction in electricProductions)
+        //                {
+        //                    double epValue = electricProduction.EP;
+        //                    DateTime dateValue = electricProduction.Date;
+
+        //                    ws.Cells[rowNumber, 1].Value = productName;
+        //                    ws.Cells[rowNumber, 2].Value = latitude;
+        //                    ws.Cells[rowNumber, 3].Value = longitude;
+        //                    ws.Cells[rowNumber, 4].Value = dateValue.ToString(CultureInfo.InvariantCulture);
+        //                    ws.Cells[rowNumber, 5].Value = epValue;
+
+        //                    rowNumber++;
+        //                }
+        //            }
+        //        }
+
+        //        fileName = "EPreport.xlsx";
+        //        package.SaveAs(new FileInfo(fileName));
+        //    }
+
+        //    //Send Email with xlsx attched
+        //    MailMessage mail = new MailMessage();
+        //    SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+        //    mail.From = new MailAddress("dbw.project.2023@gmail.com");
+        //    mail.To.Add(email);
+        //    mail.Subject = $"Electric Produces Report of Project {projectName}";
+        //    mail.Body = emailBody.ToString();
+
+        //    Attachment attachment = new Attachment(fileName);
+        //    mail.Attachments.Add(attachment);
+
+        //    //SMTP server credentials verify
+        //    SmtpServer.Port = 587;
+        //    SmtpServer.Credentials = new System.Net.NetworkCredential("dbw.project.2023@gmail.com", "rtugmlllaixpnlcj");
+        //    SmtpServer.EnableSsl = true;
+
+        //    bool isSuccess = false;
+        //    try
+        //    {
+        //        await SmtpServer.SendMailAsync(mail);
+        //        isSuccess = true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //    finally
+        //    {
+        //        // Delete the file after sending the email
+        //        //if (File.Exists(fileName)) { File.Delete(fileName); }
+        //    }
+        //    return isSuccess;
+        //}
+
         public async Task<bool> SendReport(IList<ReportData> reportData, int userId, string projectName)
         {
-            string fileName;
-
-            //Get recipient Email and project name
+            // Get recipient Email and project name
             UserDTO user = await _userAccountRepository.GetUser(userId);
             string email = user.Email;
 
+            // Set up a StringBuilder for the email body
             StringBuilder emailBody = new StringBuilder();
             emailBody.AppendLine($"Report 30 Days Electric Produce of All products of the Project: {projectName}");
 
-            //Generate Excel file, grouping product by lat and long
+            // Generate Excel file, grouping product by lat and long
             ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            string fileName = $"EPreport_{DateTime.UtcNow:yyyyMMdd_HHmmss}.xlsx"; // Use unique file name
             using (var package = new ExcelPackage())
             {
                 var groupedReportData = reportData.GroupBy(r => new { r.Product.Id });
@@ -72,44 +167,49 @@ namespace PhotovoltaicSystemCalculation.Services
                             rowNumber++;
                         }
                     }
+                    
+
+                    // Save Excel file using FileStream
+                    using (var stream = new FileStream(fileName, FileMode.Create))
+                    {
+                        package.SaveAs(stream);
+                    }
                 }
-
-                fileName = "EPreport.xlsx";
-                package.SaveAs(new FileInfo(fileName));
             }
-
-            //Send Email with xlsx attched
-            MailMessage mail = new MailMessage();
-            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-
-            mail.From = new MailAddress("dbw.project.2023@gmail.com");
-            mail.To.Add(email);
-            mail.Subject = $"Electric Produces Report of Project {projectName}";
-            mail.Body = emailBody.ToString();
-
-            Attachment attachment = new Attachment(fileName);
-            mail.Attachments.Add(attachment);
-
-            //SMTP server credentials verify
-            SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("dbw.project.2023@gmail.com", "rtugmlllaixpnlcj");
-            SmtpServer.EnableSsl = true;
 
             bool isSuccess = false;
-            try
+            using (MailMessage mail = new MailMessage())
             {
-                await SmtpServer.SendMailAsync(mail);
-                isSuccess = true;
+                using (SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com"))
+                {
+                    mail.From = new MailAddress("dbw.project.2023@gmail.com");
+                    mail.To.Add(email);
+                    mail.Subject = $"Electric Produces Report of Project {projectName}";
+                    mail.Body = emailBody.ToString();
+
+                    Attachment attachment = new Attachment(fileName);
+                    mail.Attachments.Add(attachment);
+
+                    // SMTP server credentials verify
+                    SmtpServer.Port = 587;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential("dbw.project.2023@gmail.com", "rtugmlllaixpnlcj");
+                    SmtpServer.EnableSsl = true;
+
+                    try
+                    {
+                        await SmtpServer.SendMailAsync(mail);
+                        isSuccess = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                // Delete the file after sending the email
-                //if (File.Exists(fileName)) { File.Delete(fileName); }
-            }
+
+            // Delete the file after sending the email
+            if (File.Exists(fileName)) { File.Delete(fileName); }
+
             return isSuccess;
         }
     }
