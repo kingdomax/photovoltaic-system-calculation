27d564b86162014e18a35c51b15d450b302ff8f0
kingdomax <kingdomax@windowslive.com>
2023-10-16 21:39:49 +0200

    Split job on workflow

--- backend/PhotovoltaicSystemCalculation/UnitTest/Helpers/DateTiimeHelperTest.cs
+++ backend/PhotovoltaicSystemCalculation/UnitTest/Helpers/DateTiimeHelperTest.cs
@@ -5,6 +5,18 @@ namespace UnitTest.Helpers
 {
     public class DateTimeHelperTests
     {
+        //[Theory]
+        [InlineData("2022-10-14 12:34:56.789", 1665743696)]
+        [InlineData("2022-01-01 00:00:00.000", 1640991600)]
+        public void ConvertToUnix_ValidDateTimeString_ConvertsToUnixTimestamp(string iso8601String, long expectedUnixTimestamp)
+        {
+            // Act
+            long actualUnixTimestamp = DateTimeHelper.ConvertToUnix(iso8601String);
+
+            // Assert
+            Assert.Equal(expectedUnixTimestamp, actualUnixTimestamp);
+        }
+
         [Theory]
         [InlineData("invalid-string")]
         [InlineData("2022-99-99 99:99:99.999")]
