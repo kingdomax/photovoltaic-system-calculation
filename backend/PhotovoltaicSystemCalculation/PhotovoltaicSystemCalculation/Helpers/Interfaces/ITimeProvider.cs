82934cc4c7ee59b87d8017266a60b923861474b6
kingdomax <kingdomax@windowslive.com>
2023-10-16 21:04:12 +0200

    Add test for DateTimeHelper

---
+++ backend/PhotovoltaicSystemCalculation/PhotovoltaicSystemCalculation/Helpers/Interfaces/ITimeProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace PhotovoltaicSystemCalculation.Helpers.Interfaces
+{
+    public interface ITimeProvider
+    {
+        DateTime UtcNow { get; }
+    }
+
+    public class RealTimeProvider : ITimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+
+    public class MockTimeProvider : ITimeProvider // For unit testing purpose (eg. DateTimeHelperTests)
+    {
+        public DateTime UtcNow { get; set; }
+    }
+}
